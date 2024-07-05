cd4b12ed48ba5b7f261886dde29252bf85a1e32e
Ximena Celis <112907035+Nelixao@users.noreply.github.com>
2024-07-05 16:00:55 -0600

    Add files via upload
    conversion de enteros a flotantes y diceversa

---
+++ conversion de numeros.py
@@ -0,0 +1,20 @@
+#convertidor de valores
+x=5
+
+print(type(x))
+print(x)
+
+y=float(x)
+
+print(type(y))
+print(y)
+
+
+a=3.94166567893
+print(type(a))
+print(a)
+
+
+b=int(a)
+print(type(b))
+print(b)
