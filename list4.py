6a89f8b222be9e96a14ec3ee46f03f998669e6fe
Ximena Celis <112907035+Nelixao@users.noreply.github.com>
2024-10-06 21:08:26 -0600

    Update list4.py

--- list4.py
+++ list4.py
@@ -1,1 +1,13 @@
 
+milista=[10,20,30,40,50]
+
+if 10 in milista:
+  print("el numero si s encuentra ")
+else:
+  print("no esta")
+
+
+if 12 in milista:
+  print("el numero si s encuentra ")
+else:
+  print("no esta")
